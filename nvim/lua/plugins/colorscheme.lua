c73590a08b68965948fdd3110f8599aa7237e9af
UsmanKhalil25 <usmankhalil8011@gmail.com>
2026-06-19 17:05:08 +0500

    feat: add lazyvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  "tiesen243/vercel.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("vercel").setup({
+      theme = "dark",
+      transparent = false,
+      italics = {
+        comments = true,
+        keywords = false,
+        functions = false,
+        strings = false,
+        variables = false,
+        bufferline = false,
+      },
+      overrides = {},
+    })
+    vim.cmd.colorscheme("vercel")
+  end,
+}
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      hidden = true,
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+        },
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+        grep = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+    },
+  },
+}
